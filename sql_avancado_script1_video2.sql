5bc709063d43a7b3e70f64eeaa1f84cb33a3a102
jefesonalves <jefesonbezerra@gmail.com>
2024-05-15 15:43:41 -0300

    Exercícios 3 e 4 do vídeo 2

--- sql_avancado_script1_video2.sql
+++ sql_avancado_script1_video2.sql
@@ -23,3 +23,54 @@ USING(film_id)
 JOIN category
 USING(category_id)
 GROUP BY(name);
+
+-- subquery
+SELECT title FROM film
+WHERE
+	length = (SELECT MAX(length) FROM film);
+    
+-- Exercício 3: Selecionar filmes com o tempo de aluguel acima da media
+SELECT title AS "Título", rental_duration AS "Duração" FROM film
+WHERE
+	rental_duration > (SELECT AVG(rental_duration) FROM film)
+ORDER BY(rental_duration) ASC;
+
+-- Stored procedure sem argumentos
+DELIMITER //
+CREATE PROCEDURE usuarios_ativos ()
+BEGIN
+	SELECT * FROM customer
+    WHERE active = 1;
+END//
+DELIMITER ;
+
+-- Chamar a stored procedure criada "usuarios_ativos"
+CALL usuarios_ativos()
+
+-- Stored procedure com argumentos
+DELIMITER //
+CREATE PROCEDURE get_movies_from_category (category_name VARCHAR(100))
+BEGIN
+	SELECT title AS "Título", name AS "Categoria" FROM film
+    JOIN film_category
+    USING(film_id)
+    JOIN category
+    USING(category_id)
+    WHERE name = category_name;
+END//
+DELIMITER ;
+
+-- Chamar a stored procedure criada "get_movies_from_category" com o argumento
+call get_movies_from_category("Horror")
+
+-- Exercício 4: Procedure que lista filmes maior ou igual a um rental_duration
+-- de um argumento
+DELIMITER //
+CREATE PROCEDURE get_movies_maior_igual_arg (duration INT)
+BEGIN
+	SELECT title, rental_duration FROM film
+    WHERE rental_duration >= duration;
+END//
+DELIMITER ;
+
+CALL get_movies_maior_igual_arg(2)
